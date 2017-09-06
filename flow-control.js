600f55a947c87805c191aef2c46b3dec80a91520
Jordan Johnson <jkj86@hotmail.com>
2017-09-06 05:57:24 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -33,5 +33,9 @@ return (age < 20 && age > 12) ? "You are a teenager!" : "You are not a teenager"
 }
 
 function switchAge(age) {
+switch (age){
+case 13,14,15,16,17,18,19:
+return "You are a teenager!";  
+}
 
 }
