a6afea38f098440b09f95b8851bc234ff2bedc61
Jordan Johnson <jkj86@hotmail.com>
2017-09-06 05:53:19 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -2,15 +2,6 @@ function basicTeenager(age) {
   if (age > 12 && age < 20){
     return "You are a teenager!";
   }
-  else if (age < 13) {
-    return "You are a kid";
-  }
-  else if (age > 19) {
-    return "You are a grownup";
-  }
-  else{
-    return "You are not a teenager!";
-  }
 }
 
 function teenager(age) {
