78e06e6f2a98b6955e4a6ea162a8a7d331cc6005
Jordan Johnson <jkj86@hotmail.com>
2017-09-06 05:45:34 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -10,7 +10,10 @@ function ageChecker(age) {
   if (age > 12 && age < 20){
     return "You are a teenager!";
   }
-  else {
+  else if (age < 13) {
+    return "You are a kid!";
+  }
+  else{
     return "You are not a teenager!";
   }
 }
